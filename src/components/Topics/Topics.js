a475e1f79f32f94fa495f67ad50cfe692de8b268
NahianNahin <nahian1927040@stud.kuet.ac.bd>
2022-10-12 19:17:00 +0600

    some useless code remove and last commit

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import HeadingSection from '../HeadingSection/HeadingSection';
 import Quizs from '../Quizs/Quizs';
 
 const Topics = () => {
